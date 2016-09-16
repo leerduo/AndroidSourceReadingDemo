eeff1f280c4570f8e01ac80ceed9b3fb5b849372
leerduo <eoeduo@163.com>
2016-09-16 13:49:53 +0800

    ImageLoader 1.0

--- app/src/main/java/cn/chenfuduo/androidsourcereadingdemo/MainActivity.java
+++ app/src/main/java/cn/chenfuduo/androidsourcereadingdemo/MainActivity.java
@@ -2,12 +2,19 @@ package cn.chenfuduo.androidsourcereadingdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int nThreads = Runtime.getRuntime().availableProcessors();
+        Log.e(TAG, "onCreate: " + nThreads);
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        Log.e(TAG, "onCreate: " + maxMemory );
     }
 }
--- app/src/main/java/cn/chenfuduo/androidsourcereadingdemo/SRP/ImageLoader.java
+++ app/src/main/java/cn/chenfuduo/androidsourcereadingdemo/SRP/ImageLoader.java
@@ -1,7 +1,68 @@
 package cn.chenfuduo.androidsourcereadingdemo.SRP;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.LruCache;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by chenfuduo on 2016/9/15.
  */
 public class ImageLoader {
+    //图片缓存
+    LruCache<String,Bitmap> mImageCache;
+    //线程池，线程数量为CPU的数量
+    ExecutorService mExecutorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public ImageLoader() {
+        initImageCache();
+    }
+
+    private void initImageCache() {
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        final int cacheSize = maxMemory / 4;
+        mImageCache = new LruCache<String,Bitmap>(cacheSize){
+            @Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+                return bitmap.getRowBytes() * bitmap.getHeight() / 1024;
+            }
+        };
+    }
+    public void displayImage(final String url, final ImageView imageView){
+        imageView.setTag(url);
+        mExecutorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                Bitmap bitmap = downloadBitmap(url);
+                if (bitmap == null){
+                    return;
+                }
+                if (imageView.getTag().equals(url)){
+                    imageView.setImageBitmap(bitmap);
+                }
+                mImageCache.put(url,bitmap);
+            }
+        });
+    }
+    public Bitmap downloadBitmap(String imageUrl){
+        Bitmap bitmap = null;
+        try {
+            URL url = new URL(imageUrl);
+            final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            BitmapFactory.decodeStream(conn.getInputStream());
+            conn.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
 }
